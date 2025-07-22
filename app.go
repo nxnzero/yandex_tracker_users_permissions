504f9e8e81643e746b47806235e813b420960112
solom8d <92113576+nxnzero@users.noreply.github.com>
2025-07-22 17:03:05 +0700

    Created app.go

---
+++ app.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"rightmap/queues"
+	"rightmap/rights"
+	"rightmap/users"
+)
+
+func main() {
+	file, err := os.Create("output.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	users := users.AllUsers()
+
+	for _, user := range users {
+		// fmt.Printf("Пользователь: %v \n", user.Display)
+		queues := queues.AllQueues()
+    
+		for _, v := range queues {
+			rights := rights.QueueuByUser(v.Key, user.Login)
+            // Добавить проверки, если у вас есть удаленнные очереди.
+      
+			for _, userRight := range rights {
+				line := fmt.Sprintf("Очередь: %v\t| Права доступа: %v\t| Пользователь: %v\n", v.Key, userRight, user.Display)
+				_, err := file.WriteString(line)
+				if err != nil {
+					log.Fatal(err)
+					continue
+				}
+			}
+		}
+	}
+}
